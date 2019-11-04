f32f9c218dbc56b62b61bec1653ea1faee75deec
Brian Patterson <brian@zipcoders-MacBook-Pro-5.local>
2019-11-03 19:53:18 -0500

    Completed Lab

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,37 @@
 package rocks.zipcode.io;
 
+import rocks.zipcode.io.comparators.IdComparator;
+import rocks.zipcode.io.comparators.NameComparator;
+import rocks.zipcode.io.comparators.PriceComparator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+    private Item[] items;
+
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        ArrayList<Item> itemArray = new ArrayList<Item>(Arrays.asList(items));
+        int arrayLength = items.length;
+        if (comparator.equals(new IdComparator())) {
+            Collections.sort(itemArray, new IdComparator());
+        } else if (comparator.equals(new NameComparator())) {
+            Collections.sort(itemArray, new NameComparator());
+        } else {
+            Collections.sort(itemArray, new PriceComparator());
+
+        }
+        return itemArray.toArray(new Item[arrayLength]);
+
     }
 }
